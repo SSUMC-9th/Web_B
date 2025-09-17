352007f2b0b06c8348f8e92bc81d1484ee2a99d2
AndyH0ng <chili.tomat0@icloud.com>
2025-09-17 20:10:08 +0900

    [week1/mission] 할 일과 완료 데이터 통합

--- Week1/TodoList/src/model/TodoList.ts
+++ Week1/TodoList/src/model/TodoList.ts
@@ -1,8 +1,7 @@
 import type { Todo } from './Todo';
 
 export class TodoList {
-  private activeTodos: Todo[] = [];
-  private completedTodos: Todo[] = [];
+  private todos: Todo[] = [];
   private nextId: number = 1;
 
   addTodo(title: string): void {
@@ -11,34 +10,23 @@ export class TodoList {
       title,
       completed: false
     };
-    this.activeTodos.push(newTodo);
+    this.todos.push(newTodo);
   }
 
   deleteTodo(id: number): void {
-    this.completedTodos = this.completedTodos.filter(todo => todo.id !== id);
+    this.todos = this.todos.filter(todo => todo.id !== id);
   }
 
   completeTodo(id: number): void {
-    const todo = this.activeTodos.find(t => t.id === id);
-    if (todo) {
-      const completedTodo: Todo = {
-        // 스프레드 연산자: 객체의 모든 속성을 복사
-        ...todo,
-        // completed를 true로 덮어쓰기
-        completed: true
-      };
-      // completedTodos에 추가
-      this.completedTodos.push(completedTodo);
-      // activeTodos에서 제거
-      this.activeTodos = this.activeTodos.filter(t => t.id !== id);
-    }
+    const todo = this.todos.find(t => t.id === id);
+    todo && (todo.completed = true);
   }
 
   getActiveTodos(): Todo[] {
-    return this.activeTodos;
+    return this.todos.filter(todo => !todo.completed);
   }
 
   getCompletedTodos(): Todo[] {
-    return this.completedTodos;
+    return this.todos.filter(todo => todo.completed);
   }
 }
