d5a3c1fe9b5c86b73aa3538dee952a944a424e77
AndyH0ng <chili.tomat0@icloud.com>
2025-09-23 18:17:15 +0900

    [week2/practice] 컴포넌트 실습

--- Andy/Week2/Practice/src/App.tsx
+++ Andy/Week2/Practice/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
+import List from "./components/List.tsx";
 
 function App() {
   const name = "홍준우"
   const nickname = "Andy"
   const school = "숭실대학교"
+  // as const : 리터럴 타입으로 지정해줌
+  const array = ['춘식이', '라이언', '어피치'] as const
   return (
     <>
       <strong className={"school"}>{school}</strong>
       <p className={"description"}>{`${nickname}/${name}`}</p>
+      <ul>
+        {array.map((item, index) => (
+         <List key={index} name={item}/>
+        ))}
+      </ul>
     </>
   )
 }
