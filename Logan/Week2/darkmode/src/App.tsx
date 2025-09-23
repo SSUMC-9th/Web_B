5d77880ca91d0f4f4cc27e3ef2d1d6c3d05912c6
wonellyho <navybsktb@gmail.com>
2025-09-24 01:06:36 +0900

    [week2/mission] darkmode_using_useContext

---
+++ Logan/Week2/darkmode/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+import ContextPage from './ContextPage'
+
+export default function App() {
+  return (
+    <>
+    <ContextPage/>
+      
+    </>
+  )
+}
---
+++ Logan/Week2/darkmode/src/ContextPage.tsx
@@ -0,0 +1,24 @@
+import React, { useState, type JSX } from 'react'
+import Navbar from './Navbar'
+import ThemeContent from './ThemeContent'
+import { ThemeProvider } from './context/ThemeProvider'
+
+export default function ContextPage(): JSX.Element {
+
+
+  return (
+    <ThemeProvider>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+
+            <Navbar/>
+            <main className='flex-1 w-full'>
+
+
+                <ThemeContent/>
+            </main>
+        </div>
+
+    </ThemeProvider>
+
+  )
+}
