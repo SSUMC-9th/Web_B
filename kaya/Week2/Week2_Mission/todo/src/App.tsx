4fdd2c5e1b40adc4553c50d43c1c44e39440db67
LSY <emmasylim@gmail.com>
2025-09-24 23:33:14 +0900

    [week2/mission] TodoList, DarkMode 만들기

---
+++ kaya/Week2/Week2_Mission/todo/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Todo from './components/Todo.tsx'
+
+function App() {
+  return (
+    <>
+        <Todo/>
+    </>
+  )
+}
+
+export default App
