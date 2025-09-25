d448697ee0ea8846013cc94f87dfb2335d9c8d7c
rjsckd <rjsckd020615@gmail.com>
2025-09-25 11:20:05 +0900

    mission1 추가

--- gun/Week2/Todo_list2/src/components/Todo.tsx
+++ gun/Week2/Todo_list2/src/components/Todo.tsx
@@ -1,79 +1,37 @@
-import { useState, type FormEvent } from "react";
-import type { TTodo } from "../types/todo";
 
-const Todo = () => {
-const [todos, setTodos] = useState<TTodo[]>([
-]);
-const [doneTodos, setDoneTodos] = useState<TTodo[]>([
-]);
-const [input, setInput] = useState<string>('');
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import { useTodo } from "../context/TodoContext";
 
-console.log('Input', input);
+const Todo = ()=> {
 
-const handleSubmit = (e: FormEvent<HTMLFormElement>) : void => {
-  e.preventDefault();
-  console.log('동작함')
-  const text = input.trim();
+    const {todos, completeTodo, deleteTodo, doneTodos}= useTodo();
+   
+    
 
-  if (text) {
-    const newTodo: TTodo = {id: Date.now(), text};
-    setTodos((prevTodos) : TTodo[] => [...prevTodos, newTodo]);
-    setInput('');
-  }
-};
-
-const completeTodo = (todo: TTodo) : void => {
-  setTodos(prevTodos => prevTodos.filter((t) : boolean => t.id !== todo.id));
-  setDoneTodos((prevDoneTodos) : TTodo[] => [...prevDoneTodos, todo]);
-}
-
-const deleteTodo = (todo: TTodo) : void => {
-  setDoneTodos(prevDoneTodos => prevDoneTodos.filter((t) : boolean => t.id !== todo.id));
-}
-
-
-  return (
-    <div className="todo-container">
-      <h1 className="todo-container__header">Gun TODO</h1>
-      <form onSubmit={handleSubmit} className="todo-container__form">
-        <input 
-          value={input}
-          onChange={(e) : void => setInput(e.target.value)}
-          type='text'
-          className="todo-container__input" 
-          placeholder="할 일 입력" 
-          required 
-        />
-        <button type="submit" className="todo-container__button">
-          할 일 추가
-        </button>
-      </form>
-      <div className="render-container">
-        <div className="render-container__section">
-          <h2 className="render-container__title">할 일</h2>
-          <ul id="todo-list" className="render-container__list">
-            {todos.map((todo) : any => (
-             <li key = {todo.id} className ="render-container__item">
-              <span className="render-container__item-text">{todo.text}</span>
-              <button onClick={() : void => completeTodo(todo)} style = {{ backgroundColor: '#28a745', }} className="render-container__item-button">완료</button>
-            </li>
-            ))}
-          </ul>
+    return (
+     <div className='todo-container'>
+        <h1 className="todo-container__header">Gun TODO 최신</h1>
+        <TodoForm />
+        <div className="render-container">
+        <TodoList
+            title = '할 일'
+            todos={todos}
+            buttonLabel='완료'
+            buttonColor='#28a745'
+            onClick={completeTodo}
+            />
+        <TodoList
+            title = '할 일'
+            todos={doneTodos}
+            buttonLabel='삭제'
+            buttonColor='#dc3545'
+            onClick={deleteTodo}
+            />
         </div>
-        <div className="render-container__section">
-          <h2 className="render-container__title">할 일</h2>
-                <ul id="todo-list" className="render-container__list">
-                {doneTodos.map((todo) : any => (
-                  <li key = {todo.id} className ="render-container__item">
-                    <span className="render-container__item-text">{todo.text}</span>
-                    <button onClick={() : void => deleteTodo(todo)} style = {{ backgroundColor: '#dc3545', }} className="render-container__item-button">삭제</button>
-                  </li>
-                  ))}
-              </ul>
-          </div>
-      </div>
-    </div>
-  );
+     </div>
+    );
 };
 
 export default Todo;
+
