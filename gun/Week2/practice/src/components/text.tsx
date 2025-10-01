508dea9e8ababf2e85ba503a610343dd1eb5a2f1
rjsckd <rjsckd020615@gmail.com>
2025-10-02 01:46:55 +0900

    mission3 movie_list1

---
+++ gun/Week2/practice/src/components/text.tsx
@@ -0,0 +1,10 @@
+const ttext = () => {
+    return (
+        <>
+        <h1>오늘은 몇 달?</h1>
+        </>
+    );
+};
+
+
+export default ttext;
---
+++ gun/Week3/Practice/src/App.tsx
@@ -0,0 +1,11 @@
+import SearchPage from "./pages/searchpages";
+
+export const App = () => {
+  return (
+    <div>
+      <SearchPage/>
+    </div>
+  )
+}
+
+export default App;
