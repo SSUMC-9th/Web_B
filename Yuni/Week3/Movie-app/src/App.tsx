f76c04dfd92ddb58851825b51b2d18f53d7a9ee2
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2025-10-01 22:34:50 +0900

    [Week3 mission] 미션 1 - useEffect를 활용한 영화 목록 페이지 구현

---
+++ Yuni/Week3/Movie-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { type ReactElement } from "react";
+import MoviePage from "./pages/MoviePage";
+
+const App: ReactElement = () => {
+  return (
+    <>
+    <MoviePage />
+    </>
+  );
+};
+
+export default App;
