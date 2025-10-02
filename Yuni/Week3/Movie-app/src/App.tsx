d9700e7b85a6d28b39653c06dcad77507c240178
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2025-10-02 09:39:24 +0900

    [Week3/mission] 미션 2 - React Router와 페이지네이션을 통한 동적 영화 목록 구현

--- Yuni/Week3/Movie-app/src/App.tsx
+++ Yuni/Week3/Movie-app/src/App.tsx
@@ -1,12 +1,26 @@
-import { type ReactElement } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MoviePage from "./pages/MoviePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
+import MovieDetailPage from "./pages/MovieDetailPage.tsx";
 
-const App: ReactElement = () => {
-  return (
-    <>
-    <MoviePage />
-    </>
-  );
+const router = createBrowserRouter([
+    { path: "/", 
+      element: <HomePage />,
+      errorElement: <NotFoundPage />,
+      children: [
+        { path: "movies/:category", 
+          element: <MoviePage />,
+        },
+        { path: "movies/:movieId", 
+          element: <MovieDetailPage />,
+        }
+      ] 
+    }
+]);
+
+const App = () => {
+    return <RouterProvider router={router} />;
 };
 
 export default App;
