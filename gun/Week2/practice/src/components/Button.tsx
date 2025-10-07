85d6069f3f2ca23ba118f6457fe9673571d72879
kimgunchan <104635141+kimgunchan@users.noreply.github.com>
2025-10-07 21:07:15 +0900

    Revert "GUN [week3/mission] Tailwind CSS, React Router로 movie list 제작"

--- gun/Week2/practice/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-interface buttonprops {
-  click?: () => void;
-  text: string;
-}
-const button = ({click, text} : buttonprops) => {
-  return <button onClick={click}>{text}</button>
-}
-
-export default button;
--- gun/Week3/Practice/src/pages/home.tsx
+++
@@ -1,6 +0,0 @@
-// src/pages/home.tsx
-const HomePage = () => {
-  return <h1 className = 'm-4'>Home Page 야호~!</h1>;
-};
-
-export default HomePage;
