287ba551f832a3df25732ec0db1a77e2e5a89749
wonellyho <navybsktb@gmail.com>
2025-10-07 21:10:04 +0900

    [week3/refactor] useParams를 활용한 영화 상세 페이지 개발

---
+++ Logan/Week4/MovieUsingCustomHook/src/types/credits.ts
@@ -0,0 +1,30 @@
+export type Cast = {
+  id: number;
+  name: string;
+  profile_path: string | null;
+  character: string;
+  original_name: string;
+};
+
+export type CreditResponse = {
+  id: number;
+  cast: Cast[];
+};
+
+// {
+//   "id": 550,
+//   "cast": [
+//     {
+//       "adult": false,
+//       "gender": 2,
+//       "id": 819,
+//       "known_for_department": "Acting",
+//       "name": "Edward Norton",
+//       "original_name": "Edward Norton",
+//       "popularity": 26.99,
+//       "profile_path": "/8nytsqL59SFJTVYVrN72k6qkGgJ.jpg",
+//       "cast_id": 4,
+//       "character": "The Narrator",
+//       "credit_id": "52fe4250c3a36847f80149f3",
+//       "order": 0
+//     },
