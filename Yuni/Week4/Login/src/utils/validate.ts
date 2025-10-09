aee9b604c11dd353322d6c9d4118f580b9ae0ffd
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2025-10-09 17:42:39 +0900

    [mission/week4]  회원가입 기능 구현 (react-hook-form + zod 활용)

--- Yuni/Week4/Login/src/utils/validate.ts
+++ Yuni/Week4/Login/src/utils/validate.ts
@@ -3,6 +3,12 @@ export type UserSigninInformation = {
   password: string;
 };
 
+export type UserSignupInformation = {
+  email: string;
+  password: string;
+  passwordCheck: string;
+};
+
 function validateUser(values: UserSigninInformation) {
   const errors: { email: string; password: string } = {
     email: "",
@@ -28,6 +34,33 @@ function validateUser(values: UserSigninInformation) {
 //로그인 유효성 검사
 function validateSignin(values: UserSigninInformation) {
     return validateUser(values);
+}
+
+//회원가입 유효성 검사
+export function validateSignup(values: UserSignupInformation) {
+  const errors: { email: string; password: string; passwordCheck: string } = {
+    email: "",
+    password: "",
+    passwordCheck: "",
+  };
+
+  if (
+    !/[0-9a-zA-Z]([\-.\_]?[0-9a-zA-Z])*@[0-9a-zA-Z]([\-.\_]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(
+      values.email
+    )
+  ) {
+    errors.email = "올바른 이메일 형식이 아닙니다!";
+  }
+
+  if (!(values.password.length >= 8 && values.password.length < 20)) {
+    errors.password = "비밀번호는 8~20자 사이로 입력해주세요.";
+  }
+
+  if (values.password !== values.passwordCheck) {
+    errors.passwordCheck = "비밀번호가 일치하지 않습니다.";
+  }
+
+  return errors;
 }
 
 export default validateSignin;
