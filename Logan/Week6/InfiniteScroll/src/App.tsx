3c2646383735446dcd4e8b09b0e4266ee5e8633a
wonellyho <navybsktb@gmail.com>
2025-11-03 19:49:12 +0900

    [week6/practice] 실습2-2.무한스크롤

--- Logan/Week6/InfiniteScroll/src/App.tsx
+++ Logan/Week6/InfiniteScroll/src/App.tsx
@@ -1,13 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import InfinitePostsJsonPlaceholder from "./components/InfinitePostsJsonPlaceholder";
+import InfinitePostsAutoJsonPlaceholder from "./components/InfinitePostsAutoJsonPlaceholder";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <InfinitePostsJsonPlaceholder />
+      {/* <InfinitePostsJsonPlaceholder /> */}
+      <InfinitePostsAutoJsonPlaceholder />
       {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
