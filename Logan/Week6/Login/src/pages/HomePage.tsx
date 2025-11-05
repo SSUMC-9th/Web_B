8a2d8aae6c112a524029a7d83b7df94707e785ae
wonellyho <navybsktb@gmail.com>
2025-11-06 02:46:26 +0900

    [week6/mission] useInfiniteQuery를 활용한 무한 스크롤과 뼈다구 UI

--- Logan/Week6/Login/src/pages/HomePage.tsx
+++ Logan/Week6/Login/src/pages/HomePage.tsx
@@ -1,29 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { boolean } from "zod";
 import useGetLpList from "../hooks/queries/useGetLpList";
-
+import useGetInfiniteLpList from "../hooks/queries/useGetInfiniteLpList";
+import { PAGINATION_ORDER } from "../enums/common";
+import { InView, useInView } from "react-intersection-observer"; // 추가
+import LpCardSkeleton from "../components/LpCard/LpCardSkeleton";
+import LpCardSkeletonList from "../components/LpCard/LpCardSkeletonList";
+import LpCard from "../components/LpCard/LpCard";
 const HomePage = () => {
-  const [search, setSearch] = useState("매튜");
-  const { data, isPending, isError } = useGetLpList({
-    search,
-  });
+  const [search, setSearch] = useState("");
+  // const { data, isPending, isError } = useGetLpList({
+  //   search,
+  //   limit: 50,
+  // });
 
   //console.log(data?.data.data?.map((lp) => lp.id));
   // if (!isPending) {
   //   return <div className={"mt-20"}>Loading...</div>;
   // }
+  const {
+    data: lps,
+    isFetching,
+    hasNextPage,
+    isPending,
+    fetchNextPage,
+    isError,
+  } = useGetInfiniteLpList(10, search, PAGINATION_ORDER.desc);
 
-  if (isError) {
-    return <div>Error.</div>;
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (InView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+  if (isPending) {
+    return <div className={"mt-20"}>Loading...</div>;
   }
 
+  if (isError) {
+    return <div className={"mt-20"}>Error...</div>;
+  }
+
+  console.log(lps);
+
   return (
-    <div className={"mt-20"}>
+    <div className="container mx-auto px-4 py-6">
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      {data?.map((lp) => (
-        <h1>{lp.title}</h1>
-      ))}
+      {/* {data?.map((lp) => (
+        <h1 key={lp.id}>{lp.title}</h1> */}
+      {/* ))} */}
+      {/* {lps?.pages?.map((page) => console.log(page.data.data))} */}
+      <div
+        className={
+          "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        }
+      >
+        {isPending && <LpCardSkeletonList count={20} />}
+        {lps?.pages
+          ?.map((page) => page.data.data)
+          ?.flat()
+          ?.map((lp) => (
+            <LpCard key={lp.id} lp={lp} />
+          ))}
+        {isFetching && <LpCardSkeletonList count={20} />}
+      </div>
+      <div ref={ref} className="h-2"></div>
     </div>
   );
 };
