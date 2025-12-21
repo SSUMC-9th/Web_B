2074e8f76572317695c090a0213e177d8c8de40f
LSY <emmasylim@gmail.com>
2025-12-21 21:22:01 +0900

    [week10/mission]useMemo, useCallback, memo와 Vercel 배포하기

---
+++ kaya/week10/MovieSite/src/pages/HomePage.tsx
@@ -0,0 +1,46 @@
+import {useState, useMemo, useCallback} from 'react';
+import useFetch from '../hooks/useFetch';
+import { type MovieResponse, type MovieFilters } from '../types/movie';
+import MovieList from '../components/MovieList';
+import MovieFilter from '../components/MovieFilter';
+
+export default function HomePage() {
+    const [filters, setFilters] = useState<MovieFilters>({
+        query: '',
+        include_adult: false,
+        language: "ko-KR",
+    })
+
+    // filters가 바뀔 때(query, include_adult, language) 바뀜
+    const axiosRequestConfig = useMemo(
+        (): {params: MovieFilters} => ({
+            params: filters,
+        }),
+        [filters]
+    )
+
+    // useCallback으로 감싸서 참조값이 바뀌지 않도록
+    // props를 memo로 감싸서 props가 변경된 경우만 리렌더링
+    const handleMovieFilters = useCallback((filters: MovieFilters) => {
+        setFilters(filters);
+    }, [setFilters])
+
+    const {data, error, isLoading} = useFetch<MovieResponse>('/search/movie', {
+        params: filters,
+    });
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div className='container'>
+            <MovieFilter onChange={handleMovieFilters}/>
+            {isLoading? (
+                <div>로딩 중 입니다...</div>
+            ):(
+            <MovieList movies={data?.results || []}/>
+            )}
+        </div>
+    )
+}
---
+++ kaya/week10/useCallback-memo/src/App.tsx
@@ -0,0 +1,14 @@
+import UseCallbackPage from "./useCallback-memo/UseCallbackPage"
+import UseMemoPage from './useMemo/UseMemoPage'
+
+function App() {
+  
+
+  return (
+    <>
+     <UseCallbackPage/>
+    </>
+  )
+}
+
+export default App
